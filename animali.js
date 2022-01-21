e49ba416c9cd12124bccf2040bf37472067f2bd5
Andreoli-Matteo <andreoli.matteo@iisgalvanimi.edu.it>
2022-01-21 11:50:42 +0000

    gioco

---
+++ animali.js
@@ -0,0 +1,15 @@
+class animali{
+    tipo;
+    razza;
+    nzampe;
+    img;
+    constructor(tipo,razza,nzampe){
+        this.tipo=tipo;
+        this.razza=razza;
+        this.nzampe=nzampe;
+        
+    }
+    draw(){
+        
+    }
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+import animali from "/animali.js";
+let galloimg;
+galloimg.src="gallo-236320.jpg";
+let muccaimg;
+muccaimg.src="mucca_1020x680.webp";
+let maialeimg;
+maialeimg.src="download(1).jpg";
+
